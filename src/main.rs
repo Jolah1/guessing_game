dc2daeafcab755099fda6407ebde9c5e88369a7f
jolade okunlade <jokunlade@gmail.com>
2025-05-02 08:13:41 +0100

    Feat:Guess the number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 // This is a simple number guessing game.
     // The program generates a random number between 1 and 100,
@@ -11,16 +12,33 @@ use rand::Rng;
 fn main() {
     
     println!("Guess the number!");
-    
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println! ("Hello there, Please input your guess.");
+
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        
+        };
+
         println!("You guessed: {guess}");
-}
+
+            match guess.cmp (&secret_number) {
+                Ordering::Less => println!("Oops, Too small!"),
+                Ordering::Greater => println!(" Wow, Too big!"),
+                Ordering::Equal => { 
+                    println!("Congratulations, You win!");
+                    break;
+                }
+            }
+
+        }
+    }
